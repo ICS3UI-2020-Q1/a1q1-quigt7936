f0f1af9dce03d3bdc5565375acdca310981271e5
Thomas Quigley <quigt7936@wrdsb.ca>
2020-09-14 13:34:13 +0000

    Completed the temperature converting code.

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * To convert the temperature from celsius to fahrenheit
+ * @ThomasQuigley 
  */
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creating the scanner
+    Scanner input = new Scanner(System.in);
+
+    // ask for the temperature they want to convert
+    System.out.println("Please enter a temperature you would like to convert in degrees Celsius.");
     
+    int celsius = input.nextInt();
+
+    // convert the tempature to fahrenheit
+    int fahrenheit = (celsius * 9)/5 + 32;
+    
+
+    // give them their new temperature
+    System.out.println(celsius + "C is the same as " + fahrenheit + "F");
   }
 }
